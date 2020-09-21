0073cf33ca9669085ccedbf6f6c37963618008ef
Nicolas Navarrete <nikkolasnv1997@gmail.com>
2020-09-20 23:47:20 -0300

    task form, check, uncheck tasks, delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import "./App.css";
 
-function App() {
+interface Task {
+  name: string;
+  done: boolean;
+}
+
+function App(): JSX.Element {
+  const [newTask, setNewTask] = useState("");
+  const [taskList, setTaskList] = useState<Task[]>([]);
+  const taskInput = useRef<HTMLInputElement>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTask(e.target.value);
+  };
+
+  const handleChangeTaskState = (i: number): void => {
+    const tasksCpy: Task[] = [...taskList];
+    tasksCpy[i].done = !tasksCpy[i].done;
+    setTaskList(tasksCpy);
+  };
+
+  const addTask = (name: string): void => {
+    const newTasks = [...taskList, { name, done: false }];
+    setTaskList(newTasks);
+  };
+
+  const handleDeleteTask = (i: number): void => {
+    const tasksCpy: Task[] = [...taskList];
+    tasksCpy.splice(i, 1);
+    setTaskList(tasksCpy);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    newTask.length > 0 && addTask(newTask);
+    setNewTask("");
+    taskInput.current?.focus();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1>Tasks APP</h1>
+        <div className="break"></div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={handleChange}
+            value={newTask}
+            autoFocus
+            ref={taskInput}
+          />
+          <button>Submit</button>
+        </form>
+        <section>
+          {taskList.map((task: Task, i: number) => {
+            return (
+              <h4 key={i}>
+                &middot;{" "}
+                {task.done ? (
+                  <span style={{ color: "#11AC79" }}>[Done]</span>
+                ) : (
+                  <span style={{ color: "#FFD700" }}>[Pending]</span>
+                )}{" "}
+                {task.name}{" "}
+                <button onClick={() => handleChangeTaskState(i)}>
+                  {task.done ? "✗" : "✓"}
+                </button>
+                <button onClick={() => handleDeleteTask(i)}>🗑</button>
+              </h4>
+            );
+          })}
+        </section>
+      </div>
+    </>
   );
 }
 
